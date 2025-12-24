bb5ae10e08556cef9793de2110dfc676c9bc2e40
Lachezar Kutsarov <lkutsarov@gmail.com>
2025-12-24 21:23:12 +0100

    Create Solution.ts

---
+++ Solution.ts
@@ -0,0 +1,22 @@
+
+function minOperations(input: number[]): number {
+    const NOT_OCCURRED = -1;
+    const NUMBER_OF_ELEMENTS_IN_REMOVAL_GROUP = 3;
+
+    // Alternatively: const maxValue = Math.max(...input);
+    let maxValue = 0;
+    for (let value of input) {
+        maxValue = Math.max(maxValue, value);
+    }
+
+    const mostRecentIndexOfElementOccurrence: number[] = new Array(maxValue + 1).fill(NOT_OCCURRED);
+    let startIndexOfUniqueElementsTillEnd = 0;
+
+    for (let i = 0; i < input.length; ++i) {
+        if (mostRecentIndexOfElementOccurrence[input[i]] >= startIndexOfUniqueElementsTillEnd) {
+            startIndexOfUniqueElementsTillEnd = 1 + mostRecentIndexOfElementOccurrence[input[i]];
+        }
+        mostRecentIndexOfElementOccurrence[input[i]] = i;
+    }
+    return Math.ceil(startIndexOfUniqueElementsTillEnd / NUMBER_OF_ELEMENTS_IN_REMOVAL_GROUP);
+};
